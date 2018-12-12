7125ec2fcbdb6c88177b47f35437f3d6f5f8fe9a
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-12-12 23:38:31 +0300

    add redis cache

--- direction_service/app/services/directions_calculate_service.go
+++ direction_service/app/services/directions_calculate_service.go
@@ -3,10 +3,14 @@ package services
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/go-redis/cache"
+	"github.com/go-redis/redis"
+	"github.com/vmihailenco/msgpack"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 type DirectionsCalculateService struct{}
@@ -28,9 +32,21 @@ type graphhopperResponse struct {
 }
 
 func (_ DirectionsCalculateService) Call(startPointLat, startPointLng, endPointLat, endPointLng float64) Result {
-	client := &http.Client{}
+	redisCodec := redisCodec()
+	cacheKey := fmt.Sprintf("%f,%f,%f,%f", startPointLat, startPointLng, endPointLat, endPointLng)
 
+	if result, ok := fetchCachedResult(redisCodec, cacheKey); ok {
+		return result
+	}
+	result := fetchResult(startPointLat, startPointLng, endPointLat, endPointLng)
+	go writeResultToCache(redisCodec, cacheKey, result)
+	return result
+}
+
+func fetchResult(startPointLat, startPointLng, endPointLat, endPointLng float64) Result {
+	client := &http.Client{}
 	req, err := http.NewRequest("GET", os.Getenv("GRAPHHOPPER_URL"), nil)
+
 	if err != nil {
 		log.Print(err)
 		panic(err)
@@ -48,7 +64,6 @@ func (_ DirectionsCalculateService) Call(startPointLat, startPointLng, endPointL
 	q.Add("instructions", "false")
 	q.Add("optimize", "true")
 	req.URL.RawQuery = q.Encode()
-
 	resp, err := client.Do(req)
 
 	if err != nil {
@@ -70,4 +85,46 @@ func (_ DirectionsCalculateService) Call(startPointLat, startPointLng, endPointL
 		Distance: graphhopperResponse.Paths[0].Distance,
 		Time:     graphhopperResponse.Paths[0].Time,
 	}
+}
+
+func redisCodec() *cache.Codec {
+	ring := redis.NewRing(&redis.RingOptions{
+		Addrs: map[string]string{
+			"server": os.Getenv("REDIS_URL"),
+		},
+	})
+
+	codec := &cache.Codec{
+		Redis: ring,
+
+		Marshal: func(v interface{}) ([]byte, error) {
+			return msgpack.Marshal(v)
+		},
+		Unmarshal: func(b []byte, v interface{}) error {
+			return msgpack.Unmarshal(b, v)
+		},
+	}
+
+	return codec
+}
+
+func fetchCachedResult(codec *cache.Codec, key string) (Result, bool) {
+	var result Result
+	err := codec.Get(key, &result)
+
+	if err == nil {
+		log.Printf("Successfully read from cache by key %s", key)
+		return result, true
+	}
+	log.Printf("Unsuccessfully read from cache by key %s %v", key, err)
+	return result, false
+}
+
+func writeResultToCache(codec *cache.Codec, key string, result Result) {
+	codec.Set(&cache.Item{
+		Key:        key,
+		Object:     result,
+		Expiration: time.Minute,
+	})
+	log.Printf("Successfully write to cache key %s", key)
 }
